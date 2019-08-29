ae0e69787995328998227d33bade171879af000b
1DarkLord1 <32520090+1DarkLord1@users.noreply.github.com>
2019-08-30 00:06:39 +0300

    Add main.cpp

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define f first
+#define s second
+#define all(x) (x).begin(), (x).end()
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef pair <int, int> pii;
+typedef vector <int> vi;
+typedef vector <vi> vvi;
+typedef vector < vector <pii> > graph;
+
+signed main()
+{
+   ios_base::sync_with_stdio(0);
+   cin.tie(0);
+
+   return 0;
+}
